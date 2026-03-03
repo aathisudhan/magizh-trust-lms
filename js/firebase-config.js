bf6de2216642699ff18296aa7ada7b649f5ec29d
AATHISUDHAN <aathisudhan.ai@gmail.com>
2026-03-03 16:54:17 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,13 +5,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5aJDFHXfgqFrLhvYUMrCwNY7BFB6T6Mo",
-  authDomain: "magizh-trust-lms.firebaseapp.com",
-  projectId: "magizh-trust-lms",
-  storageBucket: "magizh-trust-lms.firebasestorage.app",
-  messagingSenderId: "728154940194",
-  appId: "1:728154940194:web:8d530bdd61bc984285bb7b",
-  measurementId: "G-J18D326HV5"
+
 };
 
 export const auth = getAuth(app);
